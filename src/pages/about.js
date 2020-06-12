55b4da787f6832707c4992756198543274587e3d
himanshugoel21 <hgoel2112@gmail.com>
2020-06-12 11:03:38 +0530

    bloe

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const PageName = () => (
+  <Layout>
+    <SEO title="PageName" keywords={[`gatsby`, `application`, `react`]} />
+    Hi! This is Himanshu and welcome to my home on internet.
+  </Layout>
+)
+
+export default PageName
