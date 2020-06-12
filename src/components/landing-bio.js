52d2ef5cc931d60587c0190721aa0c8d4bb207ae
himanshugoel21 <hgoel2112@gmail.com>
2020-06-12 11:43:07 +0530

    bloe

--- src/components/landing-bio.js
+++ src/components/landing-bio.js
@@ -21,6 +21,12 @@ const Description = styled.p`
   font-size: 1.4rem;
 `
 
+const AddDescription = styled.p`
+  padding: 0;
+  margin-bottom: 1rem;
+  font-size: 1rem;
+`
+
 const NameHeader = styled.h1`
   font-size: 3.5rem;
   margin-bottom: 0;
